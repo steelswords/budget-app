f207730f17c854ad8f749b6e0dd27a995bcfa0cf
Tristan Andrus <tristan.andrus@sdl.usu.edu>
2022-02-27 19:07:08 -0700

    Allow editing budget buckets from frontend
    Signed-off-by: Tristan Andrus <tristan.andrus@sdl.usu.edu>

--- dash_frontend.py
+++ dash_frontend.py
@@ -10,11 +10,11 @@ from budget import *
 app = Dash(__name__)
 app.Title = "Budget App"
 
-db = connect()
-ensureDatabaseSchema(db)
 print(getBudgetCategories(db))
 
 money = FormatTemplate.money(2)
+
+lowerCaseMonthNames = [ m.lower() for m in list(calendar.month_name)[1:]]
 
 def getBucketPageData():
     categories = getBudgetCategories(db)
@@ -61,9 +61,9 @@ viewPage = [
 
 monthNames = list(calendar.month_name)[1:12]
 
-def getExenseData(year : int):
-    """Returns a list of lists of dictionaries representing how much was spent in each
-    category for each month. E.g. [[{'January': 126.33}, {'February': 12.77}...]]
+def getExpenseData(year : int):
+    """Returns a list of dictionaries representing how much was spent in each
+    category for each month. E.g. [{'January': 126.33}, {'February': 12.77}...]
     The inner lists are in the same order of categories as getBudgetCategories() returns"""
     result = []
     categories = getBudgetCategories(db)
@@ -80,15 +80,45 @@ def getExenseData(year : int):
     print(result)
     return result
 
+def getBucketData(year : int):
+    wholeDataFrame = pd.read_sql(f"SELECT * FROM budgetbuckets where year = {year}", sqlAlchemyEngine)
+    result = []
+    for category in wholeDataFrame['category']:
+        # Find the bucket allocation for each month
+        innerResult = {'category': category}
+        dataFrame = pd.read_sql(f"SELECT * FROM budgetbuckets where year = {year} and category = '{category}';", sqlAlchemyEngine)
+        print(dataFrame)
+        for month in lowerCaseMonthNames:
+            amountAllocated = dataFrame[month][0]
+            innerResult[month.capitalize()] = amountAllocated
+        result.append(innerResult)
+    #print(result)
+    return result
+
+#def getBucketData(year : int):
+#    dataFrame = pd.read_sql(f"SELECT * FROM budgetbuckets where year = {year};", sqlAlchemyEngine)
+#    return dataFrame.to_dict()
+    
+
+bucketColumns=(
+    [{'id': 'category', 'name': 'Category'}] +
+    #[{'id': 'January', 'name': 'January'}]
+    [{'id': m, 'name': m, 'type':'numeric', 'format':money} for m in list(calendar.month_name)[1:13]]
+)
+
 bucketPage = [
+    html.H2("Budget by Category and Month"),
+    dash_table.DataTable(
+        id='bucket-budget-edit-table',
+        columns = bucketColumns,
+        data = getBucketData(2022),
+        editable=True),
+    html.Button('Save', id='save-buckets', n_clicks=0),
+    html.Button('Copy Buckets to Next Month', id='copy-buckets', n_clicks=0),
     html.H2("Expenses by Category and Month"),
     dash_table.DataTable(
         id='bucket-year-edit-table',
-        columns=(
-            [{'id': 'category', 'name': 'Category'}] +
-            #[{'id': 'January', 'name': 'January'}]
-            [{'id': m, 'name': m, 'type':'numeric', 'format':money} for m in list(calendar.month_name)[1:13]]
-        ),
+        columns=bucketColumns,
         #data = [{'category': 
         #data=[{'category': c, 'category': 'Spent:'} for c in getBudgetCategories(db)] +
         #    [{'January': amount} for amount in range(1,14)],
@@ -97,25 +127,23 @@ bucketPage = [
         #data = [{'category': 'Food', 'March': 300, 'June': 250},
         #    {'category': 'Tristan - WTF', 'May': 300, 'July': 250},],
         editable=False),
-    html.Button('Save', id='save-buckets', n_clicks=0),
-    html.Button('Copy Buckets to Next Month', id='copy-buckets', n_clicks=0),
     html.Div(id='empty-output'),
 
     ]
 
-@app.callback(
-        Output('empty-output', 'children'),
-        Input('bucket-year-edit-table', 'data_timestamp'),
-        State('bucket-year-edit-table', 'data'))
-def updateYearBucketTable(timestamp, rows):
-    for row in rows:
-        try:
-            print("Changing row['January'] to {}".format(row['January']))
-            for element in row:
-                print('"{}"'.format(element))
-        except Exception as error:
-            print(error)
-    return ""
+#@app.callback(
+#        Output('empty-output', 'children'),
+#        Input('bucket-year-edit-table', 'data_timestamp'),
+#        State('bucket-year-edit-table', 'data'))
+#def updateYearBucketTable(timestamp, rows):
+#    for row in rows:
+#        try:
+#            print("Changing row['January'] to {}".format(row['January']))
+#            for element in row:
+#                print('"{}"'.format(element))
+#        except Exception as error:
+#            print(error)
+#    return ""
 
 app.layout = html.Div([
     html.H1('Budget'),
@@ -125,6 +153,7 @@ app.layout = html.Div([
     ])
 ])
 
+# Old example code.
 @app.callback(
     Output('table-editing-simple-output', 'figure'),
     Input('table-editing-simple', 'data'),
@@ -140,6 +169,27 @@ def display_output(rows, columns):
             } for col in columns]
         }]
     }
+
+# Callback to update budget buckets
+@app.callback(
+    Output('empty-output', 'children'),
+    State('bucket-budget-edit-table', 'data'),
+    Input('bucket-budget-edit-table', 'data_timestamp'),
+    Input('bucket-budget-edit-table', 'columns'))
+def budgetBucketCallback(data, data_timestamp, columns):
+    monthNames = list(calendar.month_name)
+    for row in data:
+        # Update the database. This gets expensive, so it might be worthwhile later to
+        # cache this data and check for changes somewhere
+        for monthNumber in range(1,13):
+            budgetedAmount = row[monthNames[monthNumber]]
+            if budgetedAmount is None:
+                budgetedAmount = 0
+            print(f"Setting {row['category']} for month {monthNumber} to {budgetedAmount}")
+            setBucketBudget(year = 2022, category = row['category'],
+                month = monthNumber, amountBudgeted = budgetedAmount)
+
+
 
 
 @app.callback(
